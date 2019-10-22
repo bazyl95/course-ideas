30762dce621b78c6c98abe35ec19ea3d6f876db0
vasilijs <v1ac95@gmail.com>
2019-10-22 18:11:29 +0300

    Initial commit

---
+++ src/main/java/com/millenial/couses/Main.java
@@ -0,0 +1,32 @@
+package com.millenial.couses;
+
+import com.millenial.couses.model.CourseIdeaDAO;
+import com.millenial.couses.model.SimpleCourseIdeaDAO;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
+
+        get("/", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.cookie("username"));
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/sign-in", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            String username = req.queryParams("username");
+            res.cookie("username", username);
+            model.put("username", username);
+            return new ModelAndView(model, "sign-in.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+}
